4a39c1648f4a04e0bde1f6a2f533115a52d78d33
Nishant3061 <nishant3061@gmail.com>
2017-11-28 14:52:53 +0530

    Added comment feature in website

--- blog/models.py
+++ blog/models.py
@@ -17,5 +17,23 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comment_set.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    """docstring for Comment."""
+    post = models.ForeignKey('blog.Post') #one can add related_name as attribute so to avoid use of comment_set
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
