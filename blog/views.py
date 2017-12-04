f5f61600d6b95df0e6caf08a4485aecc094f610e
Nishant3061 <nishant3061@gmail.com>
2017-12-05 03:04:29 +0530

    Added delete post inside website

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,17 @@ from django.utils import timezone
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    #posts = Post.objects.all()
+    # posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 @login_required
 def post_new(request):
@@ -20,12 +23,14 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            #post.published_date = timezone.now() #this line publishes post immediately
+            # post.published_date = timezone.now() #this line publishes post
+            # immediately
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
         return render(request, 'blog/post_edit.html', {'form': form})
+
 
 @login_required
 def post_edit(request, pk):
@@ -35,17 +40,20 @@ def post_edit(request, pk):
         if form.is_valid():
             # post = form.save(commit=False) # this two line is of no use
             # post.author = request.user
-            #post.published_date = timezone.now() #this line publishes post immediately
+            # post.published_date = timezone.now() #this line publishes post
+            # immediately
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
 
+
 @login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
 
 @login_required
 def post_publish(request, pk):
@@ -53,11 +61,13 @@ def post_publish(request, pk):
     post.publish()
     return redirect('post_detail', pk=pk)
 
+
 @login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
 
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -72,11 +82,13 @@ def add_comment_to_post(request, pk):
         form = CommentForm()
         return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
+
 @login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
+
 
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
